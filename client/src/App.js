043d1cccdd8816a5db9d1bc963a31aa853c2b6d9
adityapentyala <adityapentyala02@gmail.com>
2024-07-19 13:04:49 +0530

    chore: remove unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache, from} from '@apollo/client'
-import { onError } from '@apollo/client/link/error'
-import { GetBooks } from './components/GetBooks/getBooks.js'
 import { Routes, Route } from 'react-router-dom';
 import { BooksPage } from './pages/BooksPage/BooksPage.js';
 import { AuthorsPage } from './pages/AuthorsPage/AuthorsPage.js'
